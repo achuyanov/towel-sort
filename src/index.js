44d5ace43dc2e8fec80047c56190cc154348f50c
AndreiChuyanov <achuyanov@gmail.com>
2020-09-04 23:13:37 +0300

    fix: == replaced with ===

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-
-// You should implement your task here.
-
-//
-module.exports = function towelSort (arr) {
-  return (!arr) ? [] : arr.map( (val, i) => (i%2 == 0) ? val : val.reverse()).flat();
+module.exports = function towelSort(arr) {
+    return (!arr) ? [] : arr.map((val, i) => (i % 2 === 0) ? val : val.reverse()).flat();
 }
-/*
-let a = [
-  [1, 2, 4],
-  [5, 6, 7, 8],
-  [9, 12],
-];
-
-console.log(towelSort(a));*/
